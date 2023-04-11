04c207664e8c6341780cca2928ae7e71a0444893
Zixin Liu <2137744656@qq.com>
2023-04-12 01:03:56 +0800

    step6 completed

--- practical3/practical3.cpp
+++ practical3/practical3.cpp
@@ -8,11 +8,18 @@ using namespace std;
 class MyName
 { public:
 	MyName(string,string);
+	MyName();
 	MyName(string, string, string);
 	void show(ostream&);
+	MyName readFrom(istream&);
   private:
 	  string firstName, lastName,middleName; 
 };
+MyName::MyName() {
+	firstName = "";
+	lastName = "";
+	middleName = "";
+}
 MyName::MyName(std::string s1, std::string s2)
 {
 	firstName = s1;
@@ -27,11 +34,21 @@ MyName::MyName(std::string s1, std::string s2, std::string s3)
 void MyName::show(ostream&  cout) {
 	cout << firstName <<middleName<< lastName << endl;
 }
+MyName MyName::readFrom(istream& cin) {
+	cout << "Please enter the first name:";
+	cin >> firstName;
+	cout << "Please enter the middle name:";
+	cin >> middleName;
+	cout << "Please enter the last name:";
+	cin >> lastName;
+	MyName a{ firstName,lastName,middleName };
+	return a;
+}
 int main()
 {
 
-	MyName name{ "Zi", "Liu" ,"Xin"}; 
-	name.show(std::cout);
+	MyName name{};
+	name.readFrom(cin).show(cout);
 	return 0;
 }
 
