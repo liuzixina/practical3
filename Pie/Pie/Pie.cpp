827942b9681ce1c68008fb33e3bcba927ed1a3a7
Zixin Liu <2137744656@qq.com>
2023-04-14 00:39:32 +0800

    week 3 pie

---
+++ Pie/Pie/Pie.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+class Pie
+{
+public:
+	virtual std::string description()
+	{
+		return (std::string)"this is Pie";
+	}
+	virtual void tastiness() {};
+};
+class ApplePie :public Pie
+{
+public:
+	std::string description()
+	{
+		return (std::string)"ApplePie";
+	}
+	void tastiness() {};
+};
+class RaspberryPie :public Pie
+{
+public:
+	std::string description()
+	{
+		return (std::string)"RasberryPie";
+	}
+	void tastiness() {};
+};
+int main(){
+	Pie* p = NULL;
+	p = new ApplePie;
+	cout << p->description();
+	Pie* a = NULL;
+	a = new RaspberryPie;
+	cout << a->description();
+}
